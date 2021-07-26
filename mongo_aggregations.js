5a1633e15b222e53e182b17d16d125aed16d80fa
Rajesh Kancharla <rajesh.kancharla@outlook.com>
2021-07-27 08:36:27 +1000

    Update mongo_aggregations.js

--- mongo_aggregations.js
+++ mongo_aggregations.js
@@ -879,3 +879,34 @@ db.air_alliances.aggregate([
 		}
 	}
 ])
+
+
+db.air_routes.aggregate([
+  {
+    $match: {
+      airplane: /747|380/
+    }
+  },
+  {
+    $lookup: {
+      from: "air_alliances",
+      foreignField: "airlines",
+      localField: "airline.name",
+      as: "alliance"
+    }
+  },
+  {
+    $unwind: "$alliance"
+  },
+  {
+    $group: {
+      _id: "$alliance.name",
+      count: { $sum: 1 }
+    }
+  },
+  {
+    $sort: { count: -1 }
+  }
+])
+
+
