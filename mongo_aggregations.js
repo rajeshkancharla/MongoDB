9df106ab70273d525e3b277c1ac904ccf9020cb6
Rajesh Kancharla <rajesh.kancharla@outlook.com>
2021-07-31 09:05:17 +1000

    Update mongo_aggregations.js

--- mongo_aggregations.js
+++ mongo_aggregations.js
@@ -1131,3 +1131,65 @@ db.companies.aggregate( [
   }}]).pretty() 	
 
 // ###########################################################################################################################################################
+// exercise
+// get the common movies between top 10 metacritic and top 10 imdb.rating in same call to the database
+
+db.movies.aggregate([
+  {
+    $match: {
+      metacritic: { $gte: 0 },
+      "imdb.rating": { $gte: 0 }
+    }
+  },
+  {
+    $project: {
+      _id: 0,
+      metacritic: 1,
+      imdb: 1,
+      title: 1
+    }
+  },
+  {
+    $facet: {
+      top_metacritic: [
+        {
+          $sort: {
+            metacritic: -1,
+            title: 1
+          }
+        },
+        {
+          $limit: 10
+        },
+        {
+          $project: {
+            title: 1
+          }
+        }
+      ],
+      top_imdb: [
+        {
+          $sort: {
+            "imdb.rating": -1,
+            title: 1
+          }
+        },
+        {
+          $limit: 10
+        },
+        {
+          $project: {
+            title: 1
+          }
+        }
+      ]
+    }
+  },
+  {
+    $project: {
+      movies_in_both: {
+        $setIntersection: ["$top_metacritic", "$top_imdb"]
+      }
+    }
+  }
+])
